1f769da292f5e9166ef72e582a6fc40e4728926a
Kaz Ueno <kazun.ue@gmail.com>
2025-04-24 21:51:28 +0900

    add article detail page

---
+++ pages/articles/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const route = useRoute();
+console.log(route);
+</script>
+<template>
+  <div>
+    <h1>記事詳細</h1>
+    <ArticleDetail />
+    <div>
+      <NuxtLink :to="{name: 'index'}">記事一覧に戻る</NuxtLink>
+    </div>
+  </div>
+</template>
